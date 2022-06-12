1a4ad2425a03dbd451dfd5af82b932852302afeb
n787 <monikamane404@gmail.com>
2022-06-12 16:16:24 +0530

    welcome page

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
+//@refresh reset
+import { async } from '@firebase/util';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { AsyncStorage, Button, StyleSheet, Text, TextInput, View, YellowBox } from 'react-native';
 
-export default function App() {
+YellowBox.ignoreWarnings(['Setting a timer for a long period of time']);
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState('');
+
+  useEffect(() => { 
+    readUser();
+  }, []);
+
+  async function readUser() {
+    const user = await AsyncStorage.getItem('user');
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }
+
+  async function handleOnPress() {
+    console.log("User started a chat...");
+    const id = Math.random().toString(36).substring(7);
+    const user = { id, name }
+    await AsyncStorage.setItem('user', JSON.stringify(user));
+    setUser(user);
+  }
+    if (!user) {
+      return (
+        <View style={styles.container}>
+          <TextInput style={styles.input} 
+            placeholder='Enter your name'
+            value={name}
+            onChangeText={tx =>(setName(tx))}
+          />
+          <Button style={styles.btnStart} title='Start Chat' onPress={handleOnPress}/>
+        </View>
+      );
+    }
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We have user here!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +56,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 25,
   },
+  input: {
+    height: 50,
+    width: '100%',
+    borderWidth: 1,
+    padding: 15,
+    borderColor: 'gray',
+  },
+  btnStart: {
+    margin: 10,
+
+  }
+
 });
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-cqkW440w5Y2exOys3D75h5AeCGLL4To",
+  authDomain: "react-native-chat-app-12077.firebaseapp.com",
+  projectId: "react-native-chat-app-12077",
+  storageBucket: "react-native-chat-app-12077.appspot.com",
+  messagingSenderId: "90988010737",
+  appId: "1:90988010737:web:d6e982216c4d871bb1dd50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
